739b84dc0b29431809331ea8c74d8a27da6d16b0
Piotr Roslaniec <p.roslaniec@gmail.com>
2022-01-12 11:59:47 +0100

    Quick and dirty integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { ChainId, DAppProvider } from '@usedapp/core'
+import { ChainId, DAppProvider, Mumbai } from '@usedapp/core'
 import { App } from './App'
 
 const config = {
@@ -8,7 +8,7 @@ const config = {
   readOnlyUrls: {
     [ChainId.Mainnet]: 'https://mainnet.infura.io/v3/3165a249c65f4198bf57200109b8fadf',
   },
-  supportedChains: [ChainId.Goerli],
+  networks: [Mumbai],
 }
 
 ReactDOM.render(
