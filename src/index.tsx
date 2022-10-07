5406d3f30b4bf0688d28c0be42aff9db59124a36
Piotr Roslaniec <p.roslaniec@gmail.com>
2022-10-07 20:09:38 +0200

    update rpc provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Config as DappProviderConfig, DAppProvider } from '@usedapp/core'
+import { Config as DappProviderConfig, DAppProvider, Rinkeby } from '@usedapp/core'
 import { App } from './App'
 
+const PROVIDER_URL =
+  'https://hardworking-clean-gas.rinkeby.discover.quiknode.pro/17628d541fafc8312c89998c94610cf3c76613de/'
+
 const config: DappProviderConfig = {
-  // TODO: Set supported networks
-  // networks: [
-  // Mumbai,
-  // Polygon,
-  // ],
+  readOnlyChainId: Rinkeby.chainId,
+  readOnlyUrls: {
+    [Rinkeby.chainId]: PROVIDER_URL,
+  },
+  networks: [Rinkeby],
 }
 
 ReactDOM.render(
